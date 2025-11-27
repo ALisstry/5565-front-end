f4f1b5d18aa94a96d2707580650b205a71efdb84
ALisstry <alisstry@163.com>
2025-11-27 16:11:06 +0800

    fix(vite.config.js): 修改服务器host配置为0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
-  server: { host: '127.0.0.1', port: 3000 },
+  server: { host: '0.0.0.0', port: 3000 },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
